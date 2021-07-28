8fcb1881896eb37942b230752f837e7245d9313a
ZoeyF75 <zf.zoodle@gmail.com>
2021-07-27 22:44:15 -0700

    rotate through spritesheet animation and single frame display

--- cardGame/src/index.js
+++ cardGame/src/index.js
@@ -1,19 +1,38 @@
 import Phaser from 'phaser';
+import deck from './assets/deck.png'; //0 - 51 frames, 52 is backside
 
 class MyGame extends Phaser.Scene
 {
     constructor ()
     {
         super();
-    }
+    } 
 
     preload ()
     {
+        this.load.spritesheet('deck', deck, {
+            frameWidth: 1053 / 13,
+            frameHeight: 587 / 5
+          });
     }
       
     create ()
-    {
+    {  
+        this.mysprite = this.add.sprite(200, 200, 'deck').setScale(0.5);
+        this.anims.create( {
+            key: "animation",
+            frames: this.anims.generateFrameNumbers("deck"),
+            frameRate: 5,
+            repeat: -1
+        });
+        // this.mysprite.play("animation");
+        this.mysprite.setFrame(51);
         
+    }
+
+    update ()
+    {
+
     }
 }
 
